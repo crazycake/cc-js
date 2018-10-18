40a7dff72c8cb2b6f8b91a3ba41c8d5a568e8939
Nicolas Pulido <npulidom@gmail.com>
2018-10-18 17:41:16 -0300

    code refactored, deleted form serialize logic

--- src/core.js
+++ src/core.js
@@ -7,19 +7,16 @@ export default {
 
 	/**
 	 * @property modules
-	 * @type {Object}
 	 */
 	modules: {},
 
 	/**
 	 * @property timeout - XHR Max Timeout (seconds)
-	 * @type {Int}
 	 */
 	timeout: 45000,
 
 	/**
 	 * @property flashAlerts
-	 * @type {Array}
 	 */
 	flashAlerts: [],
 
@@ -27,13 +24,9 @@ export default {
 
 	/**
 	* Set modules automatically for require function
-	* @method setModules
 	* @param {Array} modules - The required modules
 	*/
 	setModules(modules = []) {
-
-		if (!modules.length)
-			return
 
 		for (let mod of modules) {
 
@@ -44,7 +37,6 @@ export default {
 
 	/**
 	 * Starter, if module has a viewModel binds it to DOM automatically
-	 * @method start
 	 * @param {Array} - The modules array
 	 */
 	start(modules = []) {
@@ -52,25 +44,19 @@ export default {
 		// check that App Global scope vars are defined
 		if (APP == undefined) throw new Error("Core -> APP global scoped var is not defined!")
 
-		let mod_name, mod, data
-
 		// call initializers
-		for (mod_name in modules) {
+		for (let name in modules) {
 
 			// check module exists
-			if (!this.modules[mod_name]) {
+			if (!this.modules[name]) {
 
-				console.warn("Core -> attempting to load an undefined module (" + mod_name + ").")
+				console.warn(`Core -> attempting to load an undefined module: ${name}.`)
 				continue
 			}
 
-			// get module
-			mod  = this.modules[mod_name]
-			data = modules[mod_name]
-
 			// check if module has init method & call it
-			if (_.isFunction(mod.init))
-				mod.init(data)
+			if (_.isFunction(this.modules[name].init))
+				this.modules[name].init(modules[name])
 		}
 
 		this.loadUI()
@@ -78,12 +64,11 @@ export default {
 
 	/**
 	 * Core load UI, called automatically after loading modules.
-	 * @method loadUI
 	 */
 	loadUI() {
 
 		// load fast click for mobile
-		if (APP.UA && APP.UA.isMobile)
+		if (APP.UA && APP.UA.isMobile && FastClick)
 			FastClick.attach(document.body)
 
 		// look for server flash messages
@@ -92,7 +77,6 @@ export default {
 
 	/**
 	* Helper Get BaseUrl
-	* @method baseUrl
 	* @param  {String} uri - Append URI if defined
 	* @return String
 	*/
@@ -103,7 +87,6 @@ export default {
 
 	/**
 	* Helper Get StaticUrl
-	* @method staticUrl
 	* @param  {String} uri - Append URI if defined
 	* @return String
 	*/
@@ -114,88 +97,64 @@ export default {
 
 	/**
 	 * Ajax request with response handler.
-	 * @method ajaxRequest
 	 * @param  {Object} request - Axios request object
-	 * @param  {Object} form - The form HTML object
+	 * @param  {Object} ctx - The html context (prevent bubble clicking)
 	 * @param  {Object} csrf - Append APP.UA CRSF token key & value
 	 * @return {Object} promise
 	 */
-	ajaxRequest(request = null, form = null, csrf = true) {
+	ajaxRequest(request = null, ctx = null, csrf = true) {
 
 		if (!request) throw new Error("Core -> ajaxRequest: invalid input request object")
 
-		let payload    = {},
-			submit_btn = null
+		let payload = {},
+			button  = null
 
 		// check form element has a form data-invalid attribute
-		if (form) {
+		if (ctx) {
 
 			// check for a non jquery object
-			form = form instanceof jQuery === false ? $(form) : form
-
-			// form data to object
-			$.each(form.serializeArray(), function() { payload[this.name] = this.value })
+			ctx = ctx instanceof jQuery === false ? $(ctx) : ctx
 
 			// disable submit button
-			submit_btn = form.find("button")
+			button = ctx.find("button")
 
-			if (submit_btn.length)
-				submit_btn.prop("disabled", true)
+			if (button.length)
+				button.prop("disabled", true)
 		}
 
 		// append CSRF token?
-		if (csrf && request.method == "POST" && !_.isNil(APP.UA.csrfKey))
+		if (request.method == "POST" && csrf && !_.isNil(APP.UA.csrfKey))
 			payload[APP.UA.csrfKey] = APP.UA.csrfValue
 
 		// set options
-		let options = _.assign({
-			method  : "GET",
-			timeout : this.timeout,
-			dataType: "json",
-		}, request)
-
-		// set payload
-		options.data = _.assign(payload, request.data)
+		let options = _.assign({ method: "GET", dataType: "json", timeout: this.timeout, data: payload }, request)
 
 		if (options.uri)
 			options.url = this.baseUrl(options.uri)
-
-		let s = this
 
 		console.log("Core -> new XHR request", options)
 
 		return $.ajax(options)
 			.then((data) => {
 
-				console.log("Core -> parsing ajax response [" + (data.status || 0) + "]", data)
+				console.log(`Core -> parsing ajax response [${data.status || 0}]`, data)
 
 				// check for response error
 				if (data.status == "error")
-					return s.parseAjaxError(data.code || 400,
-											data.error || "not defined",
-											data.message || null)
-				// redirection?
-				if (data.redirect)
-					return location.href = data.redirect
+					return this.parseAjaxError(data.code || 400, data.error || "not defined", data.message || null)
 
 				// success
-				return data.payload || {}
+				return data.redirect ? location.href = data.redirect : (data.payload || {})
 			})
 			.fail((xhr, textStatus) => {
 
-				console.warn("Core -> ajax request failed [" + options.url + "]", textStatus, xhr.responseText || "none")
-			}).
-			always(() => {
-
-				// re-enable button?
-				if (submit_btn)
-					submit_btn.prop("disabled", false)
+				console.warn(`Core -> ajax request failed [${options.url}]`, textStatus, xhr.responseText || "none")
 			})
+			.always(() => button ? button.prop("disabled", false) : true)
 	},
 
 	/**
 	 * Ajax Error Response Handler
-	 * @method parseAjaxError
 	 * @param  {Int} code - The code error
 	 * @param  {String} error - The error string
 	 * @param  {String} message - The message string
@@ -225,7 +184,6 @@ export default {
 
 	/**
 	 * Get URI parameter by name
-	 * @method getQueryString
 	 * @param {String} name - The parameter name
 	 * @param {Boolean} url - Get from window URL by default
 	 * @return {String}
@@ -248,8 +206,7 @@ export default {
 	},
 
 	/**
-	 * Get resized image path .
-	 * @method resizedImagePath
+	 * Get resized image path
 	 * Example: `./media/dj/IMAGE1.jpg?v=5`
 	 *          `./media/dj/IMAGE1_TH.jpg?v=5`
 	 * @param  {String} url - An image URL
@@ -258,19 +215,17 @@ export default {
 	 */
 	resizedImagePath(url = "", key = "TN") {
 
-		let regex   = /\.([0-9a-z]+)(?:[\?#]|$)/i,
-			new_url = url.replace(regex, "_" + key + ".$1?")
+		url = url.replace(/\.([0-9a-z]+)(?:[\?#]|$)/i, "_" + key + ".$1?")
 
 		// remove single question mark?
-		if (new_url[new_url.length - 1] == "?")
-			new_url = new_url.substring(0, new_url.length - 1)
+		if (url[url.length - 1] == "?")
+			url = url.substring(0, url.length - 1)
 
-		return new_url
+		return url
 	},
 
 	/**
 	 * Image preloader
-	 * @method preloadImages
 	 * @param  {Array} images - The source paths
 	 * @return {Array} An image object array
 	 */
@@ -291,43 +246,40 @@ export default {
 
 	/**
 	 * jQuery Ajax Handler for loading state
-	 * @method newAjaxLoadingHandler
 	 * @param  {Object} ctx - The instance context for loading state
 	 * @param  {Int} seconds - Interval time in seconds
 	 */
 	setAjaxLoadingHandler(ctx, seconds = 1500) {
 
-		let ajax_timer
+		let timer
 
-		let handler = function(opts, set_loading) {
+		let handler = function(opts, activate) {
 
 			// optional parameter to skip loading handler
 			if (opts.loading === false)
 				return
 
-			if (set_loading) {
+			if (activate) {
 
-				clearTimeout(ajax_timer)
+				clearTimeout(timer)
 
-				ajax_timer = setTimeout(() => { ctx.loading.active = true }, seconds)
+				timer = setTimeout(() => ctx.loading.active = true, seconds)
 				return
 			}
 
 			// otherwise clear timer and hide loading
-			clearTimeout(ajax_timer)
-			ctx.loading.active = false
+			clearTimeout(timer), ctx.loading.active = false
 		}
 
 		// xhr events
 		$(document)
-		 .ajaxSend((e, xhr, opts)     => { handler(opts, true)  })
-		 .ajaxError((e, xhr, opts)    => { handler(opts, false) })
-		 .ajaxComplete((e, xhr, opts) => { handler(opts, false) })
+		 .ajaxSend((e, xhr, opts)     => handler(opts, true))
+		 .ajaxError((e, xhr, opts)    => handler(opts, false))
+		 .ajaxComplete((e, xhr, opts) => handler(opts, false))
 	},
 
 	/**
 	 * Sets pending server flash messages (stored in session), loaded automatically.
-	 * @method setFlashAlerts
 	 */
 	setFlashAlerts() {
 
